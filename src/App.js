0373ec701993c51cdf7dba6980167c54287c7ea6
Roman4iks <romathebestprogramist@gmail.com>
2023-05-30 20:08:11 +0300

    Add fetch useEffect and render other pizzas excample pizzas.json

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import { Categories } from "./components/Categories";
 import { Header } from "./components/Header";
 import { Sort } from "./components/Sort";
 import "./scss/app.scss";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://64762957e607ba4797dd62ed.mockapi.io/pizza/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
@@ -16,13 +31,16 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock
-              title={"Мексиканская"}
-              price={100}
-              types={[0, 1]}
-              image="notIMG"
-              sizes={[40, 30, 20]}
-            />
+            {items.map((item, index) => (
+              <PizzaBlock
+                key={index}
+                image={item.imageUrl}
+                title={item.title}
+                price={item.price}
+                types={item.types}
+                sizes={item.sizes}
+              />
+            ))}
           </div>
         </div>
       </div>
