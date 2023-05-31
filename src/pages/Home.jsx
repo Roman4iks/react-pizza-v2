934af2086ecfe1add6106cdf3b5e5ac0dd4f7c22
Roman4iks <romathebestprogramist@gmail.com>
2023-05-31 14:22:58 +0300

    Add useEffect window scroll up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Sort } from '../components/Sort';
 import { Categories } from '../components/Categories';
 import { ContentItems } from '../components/ContentItems';
+
 export const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
       </div>
       <ContentItems />
-    </>
+    </div>
   );
 };
