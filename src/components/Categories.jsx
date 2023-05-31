4725be52992a638082f1e7c3e30a54e6bd5b8b3c
Roman4iks <romathebestprogramist@gmail.com>
2023-05-31 14:00:51 +0300

    Add category "Все" Adaptive styles categoes and sorts

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export function Categories() {
-  const [acitveCategory, setAcitveCategory] = useState(0)
-  const categories = ["Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"] 
-  
+  const [acitveCategory, setAcitveCategory] = useState(0);
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые',
+  ];
+
   return (
     <div className="categories">
       <ul>
         {categories.map((category, index) => (
-          <li key={index} onClick={() => setAcitveCategory(index)} className={acitveCategory ===  index ? 'active' : ''}>{category}</li>
+          <li
+            key={index}
+            onClick={() => setAcitveCategory(index)}
+            className={acitveCategory === index ? 'active' : ''}
+          >
+            {category}
+          </li>
         ))}
       </ul>
     </div>
