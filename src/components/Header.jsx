2fcbefda45bf9485457caafd07d103fbbf32729f
Roman4iks <romathebestprogramist@gmail.com>
2023-06-04 21:22:59 +0300

    Add CartLogic Add CartItem Add page CartEmpty Add cartSlice

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Link, useLocation } from 'react-router-dom';
 import pizzaLogo from '../assets/img/pizza-logo.svg';
 import Search from './Search';
+import { useSelector } from 'react-redux';
 
 export function Header() {
   const location = useLocation();
-
+  const { items, totalPrice } = useSelector((state) => state.cart);
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0);
   return (
     <div className="header">
       <div className="container">
@@ -20,7 +22,7 @@ export function Header() {
         {location.pathname === '/' ? <Search /> : ''}
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
+            <span>{totalPrice} UAH</span>
             <div className="button__delimiter"></div>
             <svg
               width="18"
@@ -51,7 +53,7 @@ export function Header() {
                 strokeLinejoin="round"
               />
             </svg>
-            <span>3</span>
+            <span>{totalCount}</span>
           </Link>
         </div>
       </div>
