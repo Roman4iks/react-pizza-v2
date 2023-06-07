e9ccd7b35f2742d1d235baf778f210ebb80ea2b2
Roman4iks <romathebestprogramist@gmail.com>
2023-06-07 16:21:00 +0300

    fix Search for webpack

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import debounce from 'lodash.debounce';
 import styles from './Search.module.scss';
 import { useDispatch } from 'react-redux';
