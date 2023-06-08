d0054be2a3cc6495bec0f0d7d7566bca2e58b4b5
Roman4iks <romathebestprogramist@gmail.com>
2023-06-08 17:00:55 +0300

    Typing CartNotFound

--- src/pages/CartNotFound.tsx
+++ src/pages/CartNotFound.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NotFoundBlock from '../components/NotFoundBlock';
 
-export const CartNotFound = () => {
+export const CartNotFound: React.FC = () => {
   return (
     <>
       <NotFoundBlock />
